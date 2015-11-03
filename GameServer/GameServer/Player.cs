cef7bdfa397186cf3c522da86f694c12a2becbc6
Samuel Capotosto <sam-i-am@maine.rr.com>
2015-11-03 17:07:22 -0500

    Added server sending and recieving

---
+++ GameServer/GameServer/Player.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    public class Player
+    {
+        public string username;
+        public int x, y;
+        public int cookies;
+
+        //May not need this
+        //public Data GameData { get; set; } = new Data();
+
+        Socket _incomingSocket;
+
+        public Player(Socket incomingSocket)
+        {
+            Console.WriteLine("Connected!");
+            _incomingSocket = incomingSocket;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                //NetworkStream stream = new NetworkStream(_incomingSocket);
+                StreamReader stream = new StreamReader(new NetworkStream(_incomingSocket));
+
+                while (stream != null && !stream.EndOfStream)
+                {
+                    // Parse the incoming data
+                    string line = stream.ReadLine();
+                    Console.WriteLine("Readin: " + line);
+                    Login(line);
+                    Move(line);
+                    
+                }
+            }
+            catch (Exception)
+            {
+                // Tell the player there's a network issue.
+            }
+        }
+        public void Login(string line)
+        {
+            if (line.StartsWith("LOGIN "))
+            {
+                if (line.Length >= 6)
+                {
+                    username = line.Substring(6, line.Length - 6);
+                    Console.WriteLine("Logged in as: " + username);
+                }
+            }
+        }
+        public void Move(string line)
+        {
+            if (line.StartsWith("MOVE "))
+            {
+                Console.WriteLine("Movement");
+                if (line.Length == 6)
+                {
+                    char moveDir = line[5];
+                    if (moveDir == 'u')
+                    {
+                        y++;
+                        y %= Server.gameState.GetMapSizeY();
+                        Console.WriteLine("Up");
+                    }
+                    else if (moveDir == 'd')
+                    {
+                        y--;
+                        if(y < 0)
+                            y = 11;
+                        Console.WriteLine("Down");
+                    }
+                    else if (moveDir == 'l')
+                    {
+                        x--;
+                        if (x < 0)
+                            x = 11;
+                        x %= Server.gameState.GetMapSizeX();
+                        Console.WriteLine("Left");
+                    }
+                    else if (moveDir == 'r')
+                    {
+                        x++;
+                        x %= Server.gameState.GetMapSizeX();
+                        Console.WriteLine("Right");
+                    }
+                    Server.gameState.dirty = true;
+                }
+                Console.WriteLine("Player " + username + " moved to x" + x + " y" + y);
+            }
+            else
+            {
+                InvalidCommand();
+            }
+        }
+        public void SendUpdate(string sent)
+        {
+            Console.WriteLine("Sending! " + sent);
+            _incomingSocket.Send(Encoding.ASCII.GetBytes(sent));
+        }
+        public void InvalidCommand()
+        {
+            Console.WriteLine("Invalid Command!");
+        }
+    }
+}
--- GameServer/GameServer/Server.cs
+++ GameServer/GameServer/Server.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System;
 
 namespace GameServer
 {
@@ -21,25 +22,23 @@ namespace GameServer
         public directions direction;
     }
 
-    public class Player
-    {
-        public string username;
-        public int x, y;
-        public int cookies;
-        public Player (string named, int px, int py, int ck)
-        {
-            username = named;
-            x = px;
-            y = py;
-            cookies = ck;
-        }
-    }
-
     public class Data
     {
-        public List<Player> players;
-        public List<Cookie> cookies;
+        public bool dirty;
         public bool[,] map;
+
+        public int GetMapSizeX()
+        {
+            return map.GetLength(0);
+        }
+        public int GetMapSizeY()
+        {
+            return map.GetLength(1);
+        }
+        public Data(int sizeX, int sizeY)
+        {
+            map = new bool[sizeX, sizeY];
+        }
 
         /*public Data(string mapin)
         {
@@ -50,44 +49,61 @@ namespace GameServer
     public class Server
     {
         private Socket _listeningSocket;
-        private Thread _hostThread;
-        private int _port = 80;
-        private bool _isRunning;
-
-        protected void Start()
+        private int _port = 11000;
+        public static Data gameState;
+        private List<Player> players;
+        private List<Cookie> cookies;
+        private void MainThread()
         {
-            _isRunning = true;
-            _hostThread = new Thread(RunService);
-            _hostThread.Start();
+            while (true)
+            {
+                if (gameState.dirty)
+                {
+                    string playersString = "";
+                    foreach(Player pl in players)
+                    {
+                        playersString += pl.x + "|" + pl.y + "|";
+                    }
+                    Update(playersString);
+                    gameState.dirty = false;
+                }
+            }
         }
-
-        protected void Stop()
-        {
-            _listeningSocket?.Close();
-        }
-
         private void RunService()
         {
+            players = new List<Player>();
+            gameState = new Data(12, 12);
+
+            Thread mainThread = new Thread(MainThread);
+            mainThread.Start();
+
             // Run the service
             _listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             _listeningSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), _port)); // Always localhost
             _listeningSocket.Listen(_port);
 
-            while (_isRunning)
-            {
-                // I think that we should simply modify the UpdateManager class with the extra features a server would handle
-                // once the client-side gets figured out?
+            //Debug info
+            Console.WriteLine("Listening for connections on port " + _port);
 
-                // Use modified client update manager here.
-                // UpdateManager.Something(_listeningSocket.Accept())
-                //Thread thread = new Thread(() => service.OnStart(null));
-                //thread.Start();
+            while (true)
+            {
+                Player player = new Player(_listeningSocket.Accept());
+                players.Add(player);
+                Thread thread = new Thread(() => player.Run());
+                thread.Start();
             }
         }
-
+        public void Update(string sent)
+        {
+            foreach(Player player in players)
+            {
+                player.SendUpdate(sent + "\r\n");
+            }
+        }
         public static void Main()
         {
-
+            Server server = new Server();
+            server.RunService();
         }
     }
 }
